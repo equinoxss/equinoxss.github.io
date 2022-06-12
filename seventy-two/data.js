c5eedadc3580188ba329dcc2b6db65d4a78745ed
steve smith <equionxss@gmail.com>
2022-06-12 11:39:48 -0700

    add seventy-two

---
+++ seventy-two/data.js
@@ -0,0 +1,580 @@
+function getNamesData() {
+  return [
+    {
+      id: 1,
+      lpos: 8,
+      ppos: 4,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 2,
+      lpos: 7,
+      ppos: 3,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 3,
+      lpos: 6,
+      ppos: 2,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 4,
+      lpos: 5,
+      ppos: 1,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 5,
+      lpos: 4,
+      ppos: 8,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 6,
+      lpos: 3,
+      ppos: 7,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 7,
+      lpos: 2,
+      ppos: 6,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 8,
+      lpos: 1,
+      ppos: 5,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 9,
+      lpos: 16,
+      ppos: 12,
+      nameHb: 'הזי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 10,
+      lpos: 15,
+      ppos: 11,
+      nameHb: 'אלי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 11,
+      lpos: 14,
+      ppos: 10,
+      nameHb: 'לאו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 12,
+      lpos: 13,
+      ppos: 9,
+      nameHb: 'ההע',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 13,
+      lpos: 12,
+      ppos: 16,
+      nameHb: 'יזל',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 14,
+      lpos: 11,
+      ppos: 15,
+      nameHb: 'מבה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 15,
+      lpos: 10,
+      ppos: 14,
+      nameHb: 'הרי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 16,
+      lpos: 9,
+      ppos: 13,
+      nameHb: 'הקם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 17,
+      lpos: 24,
+      ppos: 20,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 18,
+      lpos: 23,
+      ppos: 19,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 19,
+      lpos: 22,
+      ppos: 18,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 20,
+      lpos: 21,
+      ppos: 17,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 21,
+      lpos: 20,
+      ppos: 24,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 22,
+      lpos: 19,
+      ppos: 23,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 23,
+      lpos: 18,
+      ppos: 22,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 24,
+      lpos: 17,
+      ppos: 21,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 25,
+      lpos: 32,
+      ppos: 28,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 26,
+      lpos: 31,
+      ppos: 27,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 27,
+      lpos: 30,
+      ppos: 26,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 28,
+      lpos: 29,
+      ppos: 26,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 29,
+      lpos: 28,
+      ppos: 32,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 30,
+      lpos: 27,
+      ppos: 31,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 31,
+      lpos: 26,
+      ppos: 30,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 32,
+      lpos: 25,
+      ppos: 29,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 33,
+      lpos: 40,
+      ppos: 36,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 34,
+      lpos: 39,
+      ppos: 35,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 35,
+      lpos: 38,
+      ppos: 34,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 36,
+      lpos: 37,
+      ppos: 33,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 37,
+      lpos: 36,
+      ppos: 40,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 38,
+      lpos: 35,
+      ppos: 39,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 39,
+      lpos: 34,
+      ppos: 38,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 40,
+      lpos: 33,
+      ppos: 37,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 41,
+      lpos: 48,
+      ppos: 44,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 42,
+      lpos: 47,
+      ppos: 43,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 43,
+      lpos: 46,
+      ppos: 42,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 44,
+      lpos: 45,
+      ppos: 41,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 45,
+      lpos: 44,
+      ppos: 48,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 46,
+      lpos: 43,
+      ppos: 47,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 47,
+      lpos: 42,
+      ppos: 46,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 48,
+      lpos: 41,
+      ppos: 45,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 49,
+      lpos: 56,
+      ppos: 52,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 50,
+      lpos: 55,
+      ppos: 51,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 51,
+      lpos: 54,
+      ppos: 50,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 52,
+      lpos: 53,
+      ppos: 49,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 53,
+      lpos: 52,
+      ppos: 56,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 54,
+      lpos: 51,
+      ppos: 55,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 55,
+      lpos: 50,
+      ppos: 54,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 56,
+      lpos: 49,
+      ppos: 53,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 57,
+      lpos: 64,
+      ppos: 60,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 58,
+      lpos: 63,
+      ppos: 59,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 59,
+      lpos: 62,
+      ppos: 58,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 60,
+      lpos: 61,
+      ppos: 57,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 61,
+      lpos: 60,
+      ppos: 64,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 62,
+      lpos: 59,
+      ppos: 63,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 63,
+      lpos: 58,
+      ppos: 62,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 64,
+      lpos: 57,
+      ppos: 61,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 65,
+      lpos: 72,
+      ppos: 68,
+      nameHb: 'והו',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 66,
+      lpos: 71,
+      ppos: 67,
+      nameHb: 'ילי',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 67,
+      lpos: 70,
+      ppos: 66,
+      nameHb: 'סיט',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 68,
+      lpos: 69,
+      ppos: 65,
+      nameHb: 'עלם',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 69,
+      lpos: 68,
+      ppos: 72,
+      nameHb: 'מהש',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 70,
+      lpos: 67,
+      ppos: 71,
+      nameHb: 'ללה',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 71,
+      lpos: 66,
+      ppos: 70,
+      nameHb: 'אכא',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    },
+    {
+      id: 72,
+      lpos: 65,
+      ppos: 69,
+      nameHb: 'כהת',
+      nameEn: 'Shin Vav Hei',
+      descriptionEn: 'Lorem ipsum dolar'
+    }
+  ];
+}
---
+++ seventy-two/index.js
@@ -0,0 +1,228 @@
+let selected = null;
+let ibound = false;
+let bbound = false;
+let sbound = false;
+let names = [];
+let orientation = 'portrait';
+
+function initApp() {
+  document.querySelector('#settings').addEventListener('click', () => settings());
+  setFont();
+}
+
+function initOrientation() {
+  orientation = window.innerWidth < window.innerHeight ? 'portait' : 'landscape';
+
+  const mql = window.matchMedia("(orientation: portrait)");
+  mql.addListener(m => {
+    orientation = m.matches ? 'portait' : 'landscape';
+    calculateSizes();
+    initTable();
+  });  
+}
+
+function initTable() {
+  const data = getNamesData();
+  names = orientation === 'portait'
+            ? data.sort((a,b) => a.ppos - b.ppos)
+            : data.sort((a,b) => a.lpos - b.lpos);
+
+  const el = document.querySelector('.page .names-table');
+  const cells = names.map(cell => `
+    <div class="name-cell hebrew-name small" data-id="${cell.id}">${cell.nameHb}</div>
+  `);
+
+  el.innerHTML = cells.join('');
+
+  el.addEventListener('click', event => {
+    document.querySelector('.page.info').classList.remove('hidden');
+    document.querySelector('.footer.info').classList.remove('hidden');
+    showInfo(Number(event.target.dataset.id));
+  });
+}
+
+function calculateSizes() {
+  const el = document.querySelector('style#custom');
+  el && el.remove();
+
+  const element = document.createElement('style');
+  element.id = 'custom';
+  document.head.appendChild(element);
+  const sheet = element.sheet;
+
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+
+  if (w > h) {
+    orientation = 'landscape';
+    w = w / 2;
+    h = h - 50;
+  } else {
+    h = (h - 50) / 2;
+  }
+
+  sheet.insertRule(`
+    .column {
+      width: ${w}px;
+      height: ${h}px;
+    }
+  `);
+
+  const ch = Math.floor((h-20)/9); // 20px for borders
+  sheet.insertRule(`
+    .name-cell {
+      height: ${ch}px;
+      line-height: ${ch}px;
+    }
+  `);
+}
+
+function bindPageElements() {
+  document.querySelector('#meditate-now').addEventListener('click', () => startMeditation());
+  document.querySelectorAll('.font-wrap .font-button').forEach(btn => btn.addEventListener('click', chooseFont));
+  document.querySelectorAll('.font-wrap .font-button').forEach(btn => btn.addEventListener('mousedown', ev => ev.target.classList.add('pressed')));
+  document.querySelectorAll('.font-wrap .font-button').forEach(btn => btn.addEventListener('mouseup', ev => ev.target.classList.remove('pressed')));
+}
+
+function bindElements() {
+  ibound = true;
+  document.querySelector('.footer #previous').addEventListener('click', () => previous());
+  document.querySelector('.footer #home').addEventListener('click', () => home());
+  document.querySelector('.footer #meditate').addEventListener('click', () => meditate());
+  document.querySelector('.footer #next').addEventListener('click', () => next());
+}
+
+function showInfo(id) {
+  !ibound && bindElements();
+
+  selected = names.find( n => n.id === id);
+  setBoundFields('info');
+}
+
+function setBoundFields(page) {
+  const fields = document.querySelectorAll(`.page.${page} [data-bind]`);
+  fields.forEach( field => {
+    field.innerText = selected[field.dataset.bind];
+  });
+}
+
+function home() {
+  document.querySelector('.page.info').classList.add('hidden');
+  document.querySelector('.footer.info').classList.add('hidden');
+}
+
+function previous() {
+  showInfo( Math.max(selected.id - 1, 1) );
+}
+
+function next() {
+  showInfo( Math.min(selected.id + 1, 72) );
+}
+
+function back(from) {
+  document.querySelector('.footer.back').classList.add('hidden');
+
+  if (from === 'meditate') {
+    document.querySelector('.page.meditate').classList.add('hidden');
+    document.querySelector('.page.meditate-time').classList.add('hidden');
+  } else if (from === 'settings') {
+    document.querySelector('.page.settings').classList.add('hidden');
+  }
+}
+
+function meditate() {
+  document.querySelector('.page.meditate').classList.remove('hidden');
+  document.querySelector('.page.meditate-time').classList.remove('hidden');
+  document.querySelector('.footer.back').classList.remove('hidden');
+  
+  if (!bbound) {
+    bbound = true;
+    document.querySelector('.footer #back').addEventListener('click', () => back('meditate'));
+  }
+}
+
+function startMeditation() {
+  document.querySelector('.page.meditate-time').classList.add('hidden');
+  document.querySelector('.footer.back').classList.add('hidden');
+  setBoundFields('meditate');
+
+  const ms = Number(document.querySelector('#meditate-time').value) * 1000;
+  setTimeout(() => {
+    // black
+    document.querySelector('.page.meditate').classList.add('invert');
+    document.querySelector('.page.meditate').classList.add('black');
+
+    setTimeout(() => {
+      // inverted
+      document.querySelector('.page.meditate').classList.remove('black');
+
+      setTimeout(() => {
+        // black
+        document.querySelector('.page.meditate').classList.add('black');
+    
+        setTimeout(() => {
+          // done
+          document.querySelector('.page.meditate').classList.remove('black');
+          document.querySelector('.page.meditate').classList.remove('invert');
+          document.querySelector('.page.meditate').classList.add('hidden');
+        
+        }, ms)
+      }, ms)
+    }, ms);
+  }, ms);
+}
+
+function settings() {
+  document.querySelector('.page.settings').classList.remove('hidden');
+  document.querySelector('.footer.back').classList.remove('hidden');
+
+  if (!sbound) {
+    sbound = true;
+    document.querySelector('.footer #back').addEventListener('click', () => back('settings'));
+  }  
+}
+
+function chooseFont(ev) {
+  document.querySelector('.page.settings').classList.add('hidden');
+  document.querySelector('.footer.back').classList.add('hidden');
+
+  localStorage.font = ev.target.dataset.id;
+  setFont();
+}
+
+function setFont() {
+  const el = document.querySelector('style#font');
+  el && el.remove();
+
+  const fontId = localStorage.font;
+  const fontNames = {
+    ash: 'StamAshkenaz',
+    sef: 'StamSefarad',
+    lib: 'FRLibre'
+  };
+
+  if (fontNames[fontId]) {
+    try {
+      const element = document.createElement('style');
+      element.id = 'font';
+      document.head.appendChild(element);
+      const sheet = element.sheet;
+
+      sheet.insertRule(`
+        .hebrew-name {
+          font-family: ${fontNames[fontId]};
+        }
+      `, 0);
+    } catch(e) {
+      // ignore
+    }
+  }
+}
+
+// =============
+
+initApp();
+initOrientation();
+calculateSizes();
+initTable();
+bindPageElements();
